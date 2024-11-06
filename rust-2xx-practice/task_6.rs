2fadcd78f8d5e13137359d112d11aaa1d89b9320
AnastasiaPrisyaghna <anastasia.pr@e-u.edu.ua>
2024-11-07 01:02:18 +0200

    Додано папку з репозиторію Practice: 3. Variables - 6.09.2024

---
+++ rust-2xx-practice/task_6.rs
@@ -0,0 +1,11 @@
+// Add your task description and solution here.
+fn main() {
+    let x: i32 = 10;
+    let mut y: i32 = 5;
+
+    // Modify y to 7
+    y = 7;
+
+    // Print the updated values
+    println!("The value of x is {} and the value of y is {}", x, y);
+}
