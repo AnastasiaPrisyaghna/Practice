d3f42ab47d6ed929abf730b083e6e33ca0f0d423
AnastasiaPrisyaghna <anastasia.pr@e-u.edu.ua>
2024-11-21 03:29:23 +0200

    10.4/3

---
+++ 10. Generics and Traits - 27.09.2024/10.4/task3.rs
@@ -0,0 +1,20 @@
+// FILL in the blanks.
+trait Draw {
+    fn draw(&self) -> String;
+}
+
+impl Draw for u8 {
+    fn draw(&self) -> String {
+        format!("u8: {}", *self)
+    }
+}
+
+impl Draw for f64 {
+    fn draw(&self) -> String {
+        format!("f64: {}", *self)
+    }
+}
+
+fn main() {
+    let x = 1.1f64;
+    let y = 8u8;
