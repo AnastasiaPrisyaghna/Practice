dcdf23a7c58c37aaee4ddf7a8954abd379bf5432
AnastasiaPrisyaghna <anastasia.pr@e-u.edu.ua>
2024-11-21 02:37:28 +0200

    Fix task 11 - fill in blank to match the final count value

---
+++ 7. Flow Control - 20.09.2024/task11.rs
@@ -0,0 +1,28 @@
+fn main() {
+    let mut count = 0;
+    'outer: loop {
+        'inner1: loop {
+            if count >= 20 {
+                // This would break only the inner1 loop
+                break 'inner1; // `break` also works.
+            }
+            count += 2;
+        }
+
+        count += 5;
+
+        'inner2: loop {
+            if count >= 30 {
+                // This breaks the outer loop
+                break 'outer;
+            }
+
+            // This will continue the outer loop
+            continue 'outer;
+        }
+    }
+
+    assert!(count == 30); // Fill in the blank: `count` should be 30 when the loop ends
+
+    println!("Success!");
+}
