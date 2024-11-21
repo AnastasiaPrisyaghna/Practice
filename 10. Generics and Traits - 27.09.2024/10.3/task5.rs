70ae7b91a394299a28b7a51b11796a5512f64db1
AnastasiaPrisyaghna <anastasia.pr@e-u.edu.ua>
2024-11-21 03:23:12 +0200

    10.3/5

---
+++ 10. Generics and Traits - 27.09.2024/10.3/task5.rs
@@ -0,0 +1,32 @@
+trait Summary {
+    fn summarize(&self) -> String;
+}
+
+#[derive(Debug)]
+struct Post {
+    title: String,
+    author: String,
+    content: String,
+}
+
+impl Summary for Post {
+    fn summarize(&self) -> String {
+        format!("The author of post {} is {}", self.title, self.author)
+    }
+}
+
+#[derive(Debug)]
+struct Weibo {
+    username: String,
+    content: String,
+}
+
+impl Summary for Weibo {
+    fn summarize(&self) -> String {
+        format!("{} published a weibo {}", self.username, self.content)
+    }
+}
+
+// Implement the summary function for types that implement the Summary trait.
+fn summary<T: Summary>(item: T) {
+    println!("{}", item.summarize());
