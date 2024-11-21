3f5d5c2f71478ef2332f86e3af6ebfde5cb52b28
AnastasiaPrisyaghna <anastasia.pr@e-u.edu.ua>
2024-11-21 03:26:19 +0200

    10.3/9

---
+++ 10. Generics and Traits - 27.09.2024/10.3/task9.rs
@@ -0,0 +1,17 @@
+fn example1() {
+    // `T: Trait` is the commonly used way.
+    // `T: Fn(u32) -> u32` specifies that we can only pass a closure to `T`.
+    struct Cacher<T: Fn(u32) -> u32> {
+        calculation: T,
+        value: Option<u32>,
+    }
+
+    impl<T: Fn(u32) -> u32> Cacher<T> {
+        fn new(calculation: T) -> Cacher<T> {
+            Cacher {
+                calculation,
+                value: None,
+            }
+        }
+
+        fn value(&mut self, arg: u32) -> u32 {
